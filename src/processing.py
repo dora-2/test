74ca28e30e8f86b0e4959d9b3a89476e150a7c80
dora <zyazevad@mail.ru>
2024-10-10 18:26:25 +0300

    cd

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,14 @@
-def filter_by_state(dictionary: list, state = ['EXECUTED'], new_dictionary = []) -> list:
+
+def filter_by_state(dictionary: list, state=['EXECUTED']) -> list:
     """сортировка по state"""
+    new_dictionary = []
     for i in dictionary:
-      if i['state'] in state:
-        new_dictionary.append(i)
+        if i['state'] in state:
+            new_dictionary.append(i)
     return new_dictionary
 
 
-def sort_by_date(dictionary_2: list, reverse = True):
+def sort_by_date(dictionary_2: list, reverse=True) -> list:
     """сортировка по date"""
-    dictionary_2.sort( key = lambda x: x['date'], reverse = reverse)
+    dictionary_2.sort(key=lambda x: x['date'], reverse=reverse)
     return dictionary_2
