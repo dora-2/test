8fe98aae8330863d95d516ef939e526eda44697d
dora <zyazevad@mail.ru>
2024-11-01 22:56:23 +0300

    cd6

---
+++ src/external_api.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def currency_api(to_api: str, from_api: str, amount_api: int) -> dict:
+    """ конвертирует валюту """
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_api}&from={from_api}&amount={amount_api}"
+
+    payload = {}
+    headers = {
+      "apikey": "gWrq9YL2oRi7gqCKopn7cWC68cyw1FYl"
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    result = response.text
+
+    print(f'Result: {result}')
+
+
+currency_api('RUB', 'EUR', 500)
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,33 @@
 import json
+# with open('operations.json') as f:
+#     data = json.load(f)
+from pathlib import Path
 
 
-with open('operations.json') as f:
-    data = json.load(f)
+def load_financial_transactions(file_path: str) -> list:
 
-if type(data) == list:
-    print(data)  # data - словарь, тип dict
+    if not Path(file_path).exists():
+        print(f"Файл {file_path} не найден")
+        return []
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        if isinstance(data, list):
+            return data
+        else:
+            print(f"Данные в файле {file_path} имеют неверный формат")
+            return []
+    except json.JSONDecodeError as e:
+        print(f"Произошла ошибка при разборе JSON: {e}")
+        return []
+
+
+transactions = load_financial_transactions('operations.json')
+if transactions:
+    for transaction in transactions:
+        if len(transaction) > 1:
+            print(transaction['date'], transaction["operationAmount"]["amount"], transaction['description'])
 else:
-    print([])
-
-
+    print("Список транзакций пуст")
