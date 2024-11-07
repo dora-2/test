af6799046371bb261edced36f86a12f7d7cf63a2
dora <zyazevad@mail.ru>
2024-11-07 14:27:34 +0300

    cd6

--- src/re_collections_random.py
+++ src/re_collections_random.py
@@ -1,95 +1,25 @@
 import re
-# from src.csv_pandas import csv_read
+from src.csv_pandas import excel_read
 
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
-def filter_operations(transactions, search_string):
-    # Создаем регулярное выражение для поиска строки без учета регистра
-    pattern = re.compile(search_string)
 
-    # Фильтруем операции, где в описании содержится искомая строка
-    filtered_transactions = [
-        transaction for transaction in transactions
-        if pattern.search(transaction.get('description', ''))
-    ]
+def filter_operations(tra:list, search_string:str, s=[]):
+    for i in tra:
+        if i['description'] == search_string:
+            s.append(i)
+    return s
 
-    return filtered_transactions
+# d = excel_read('transactions_excel.xlsx')
+# print(filter_operations(d, 'Перевод с карты на карту'))
 
-print(filter_operations(transactions, 'EXECUTED'))
+def filter_operations_2(tra:list, operation:list, a=0, w={}):
+    for i in operation:
+        for j in tra:
+            if j['description'] == i:
+                a += 1
+        w[i] = a
+        a = 0
+    return w
+
+
+# d = excel_read('transactions_excel.xlsx')
+# print(filter_operations_2(d, ['Перевод с карты на карту', 'Открытие вклада', 'Перевод со счета на счет'])) #587
