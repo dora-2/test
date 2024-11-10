effc1e920177b8957972e32208547c29721f5697
dora <zyazevad@mail.ru>
2024-11-10 13:13:03 +0300

    cd6

--- src/re_collections_random.py
+++ src/re_collections_random.py
@@ -1,4 +1,5 @@
 from src.csv_pandas import excel_read
+from collections import Counter
 
 
 def filter_operations(tra: list, search_string: str, s=[]) -> list:
@@ -13,17 +14,17 @@ d = excel_read('transactions_excel.xlsx')
 print(filter_operations(d, 'Перевод с карты на карту'))
 
 
-def filter_operations_2(tra: list, operation: list, a=0, w={}) -> dict:
+def filter_operations_2(tra: list, operation: list) -> dict:
     """возвращать словарь, в котором ключи — это названия категорий,
     а значения — это количество операций в каждой категории"""
-    for i in operation:
-        for j in tra:
-            if j['description'] == i:
-                a += 1
-        w[i] = a
-        a = 0
-    return w
+    counter = Counter()
+
+    for i in tra:
+        if 'description' in i and i['description'] in operation:
+            counter.update([i['description']])
+
+    return dict(counter)
 
 
 # d = excel_read('transactions_excel.xlsx')
-print(filter_operations_2(d, ['Перевод с карты на карту', 'Открытие вклада', 'Перевод со счета на счет'])) #587
+print(filter_operations_2(d, ['Перевод с карты на карту', 'Открытие вклада', 'Перевод со счета на счет']))
